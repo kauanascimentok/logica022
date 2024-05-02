e8f8e7921b5f55102ca33800a5020c0670f5532b
kauanascimentok <kaua.nascimento7@aluno.senai.br>
2024-05-02 14:08:03 -0500

    subindo arquivo

---
+++ script.js
@@ -0,0 +1,12 @@
+let idade = prompt("insira sua idade") 
+ if (idade <0 ) {
+    alert ("insira idade invalida")
+ } else  if (idade <=17){
+    alert ("você e menor de idade")
+ }
+ else if (idade >=18){
+    alert ("você e maior de idade")
+ }
+ else if (idade >=65){
+   alert("você e idoso")
+ }
